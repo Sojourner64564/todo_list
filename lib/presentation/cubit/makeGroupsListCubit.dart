74b24bd1aad6f3a19b7aee0cd242e7d82d9b5a04
Sojourner64564 <122652562+Sojourner64564@users.noreply.github.com>
2023-05-07 04:18:02 +0300

    aaaaa

--- lib/presentation/cubit/makeGroupsListCubit.dart
+++ lib/presentation/cubit/makeGroupsListCubit.dart
@@ -1,52 +1,61 @@
-import 'package:auto_route/auto_route.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hive/hive.dart';
-import 'package:todo_list/presentation/cubit/cubit.dart';
-import 'package:todo_list/presentation/cubit/tasksUpdateCubit.dart';
-
 import '../../domain/data_provider/box_manager.dart';
 import '../../domain/entity/group.dart';
 import '../../domain/entity/task.dart';
-import '../../providers/routes/routes.gr.dart';
 
-class MakeGroupsListCubit extends Cubit<List<String>>{
+class MakeGroupsListCubit extends Cubit<List<String>> {
   MakeGroupsListCubit() : super([]);
   int groupKey = 0;
   //int groupKeyForGroupWidget = 0;
   // int indexForGroups = 0;
-  var taskText='';
+  var taskText = '';
   var tasks = <Task>[];
   var newTasks = <Task>[];
-  int groups=0;
+  int groups = 0;
 //  String? firstTask;
-  var firstTasks = <String>['загрузка...','загрузка...','загрузка...','загрузка...','загрузка...'];
-final emptyList = <Task>[];
-String myString = 'huilo';
+  var firstTasks = <String>[
+    // 'загрузка...',
+    // 'загрузка...',
+    // 'загрузка...',
+    // 'загрузка...',
+    // 'загрузка...',
+  ];
 
-  void getAmountOfGroups(List<Group> groupsLocal){
+  void controlTheList(List<Group> groupsLocal){
+    for(int i=0;i<groupsLocal.length;i++){
+      firstTasks.add('загрузка...');
+    }
+  }
+
+  void incrementInList(){
+    firstTasks.add('загрузка...');
+  }
+  void decrementInList(){
+    firstTasks.removeLast();
+  }
+
+  void getAmountOfGroups(List<Group> groupsLocal) {
     groups = groupsLocal.toList().length;
-    print('$groups ====getAmountOfGroups его сообщение=============================================================');
-    for(int i=0;i<groups;i++){
+    controlTheList(groupsLocal);
+    // print('$groups ====getAmountOfGroups его сообщение=============================================================');
+    for (int i = 0; i < groups; i++) {
       initInGroupsState(i);
     }
   }
 
-  void initInGroupsState(int index) async{
+  void initInGroupsState(int index) async {
     final groupBox = await BoxManager.instance.openGroupBox();
-     final localGroupKey = groupBox.keyAt(index) as int;
-      final taskBox = await BoxManager.instance.openTaskBox(localGroupKey);
-      newTasks = taskBox.values.toList();
-     // print(newTasks.toString() + index.toString() + 'ffffffffffffffffffffffffffffff');
-      print('our index ' + index.toString());
-print(' - ' + (newTasks[0] != null ? newTasks[0].text : myString).toString() + index.toString());
-      firstTasks[index] = (' - ' + (newTasks[0].text.isNotEmpty ? newTasks[0].text: 'empty').toString());
-       emit(firstTasks.toList());
-  }
-
-  void fillList(){
+    final localGroupKey = groupBox.keyAt(index) as int;
+    final taskBox = await BoxManager.instance.openTaskBox(localGroupKey);
+    newTasks = taskBox.values.toList();
+    // print(newTasks.toString() + index.toString() + 'ffffffffffffffffffffffffffffff');
+    //print('our index ' + index.toString());
+    // print(' - ' + (newTasks.length != 0 ? newTasks[0].text : myString).toString() + index.toString());
+    firstTasks[index] = (' - ' + (newTasks.length != 0 ? newTasks[0].text : 'пустая группа').toString());
     emit(firstTasks.toList());
   }
 
-
+  void fillList() {
+    emit(firstTasks.toList());
+  }
 }
